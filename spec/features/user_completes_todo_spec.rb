d936146221bdbeb69a29d0f566f2e76caf667c8e
Eduardo Gutierrez <emaurogtz@hotmail.es>
2022-02-21 22:26:20 -0600

    Refactoring code

--- spec/features/user_completes_todo_spec.rb
+++ spec/features/user_completes_todo_spec.rb
@@ -3,13 +3,8 @@ require "rails_helper"
 feature "User completes todo" do
   scenario "Mark as complete todos" do
     sign_in
-
-    click_on "Add a new todo"
-    fill_in "Title", with: "Buy water"
-    click_on "Submit"
-
+    create_todo "Buy water"
     click_on "Mark complete"
-
-    expect(page).to have_css ".todos li.completed", text: "Buy water"
+    expect(page).to display_completed_todo "Buy water"
   end
 end
--- spec/features/user_creates_todo_spec.rb
+++ spec/features/user_creates_todo_spec.rb
@@ -3,11 +3,7 @@ require "rails_helper"
 feature "User creates todo" do
   scenario "successfully" do
     sign_in
-
-    click_on "Add a new todo"
-    fill_in "Title", with: "Buy water"
-    click_on "Submit"
-
-    expect(page).to have_css ".todos li", text: "Buy water"
+    create_todo "Buy water"
+    expect(page).to display_todo "Buy water"
   end
 end
